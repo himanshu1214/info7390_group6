2f38d6947ec50bc525dd8fca046a27fff96964a9
Xinglong <jia.xi@husky.neu.edu>
2018-10-08 20:55:10 -0400

    update parse_file

--- Assignment1/parse_file.py
+++ Assignment1/parse_file.py
@@ -3,21 +3,23 @@ from bs4 import BeautifulSoup
 import csv
 import pandas as pd
 import os
+import logging
+from configparser import ConfigParser
 
-# # IBM
-# # company ID
-# CIK = '51143' 
-# # document accession number
-# acc_no = '0000051143-13-000007'
+# check result and cleaned directory    
+outdir = './result/cleaned_tables'
+if not os.path.exists(outdir):
+    os.makedirs(outdir)
+logging.info('Check cleaned directory.')
 
-CIK = input('Enter the CIK number:')
-acc_no = input('Enter the accession number:')
+logging.basicConfig(filename='./result/parse_file.log', filemode='w', level=logging.DEBUG)
 
-# Ggogle
-# #company ID
-# CIK = '1288776' 
-# #document accession number
-# acc_no = '0001288776-15-000046'
+# read parameters CIK and acc_no from config file
+config = ConfigParser()
+config.read('config.ini')
+CIK = config.get('IBM','CIK')
+acc_no = config.get('IBM','acc_no')
+logging.info('Read parameters CIK and acc_no from config file.')
 
 # replace '_' in accession number
 string1 = acc_no.replace('-','')
@@ -25,6 +27,7 @@ string1 = acc_no.replace('-','')
 # generate index url and parse index html
 indexUrl = 'https://www.sec.gov/Archives/edgar/data/' + CIK + '/' + string1 + '/' + acc_no + '-index.html'
 indexPage = BeautifulSoup(urlopen(indexUrl),'lxml')
+logging.info('Generate index url and parse index html.')
 
 # find 10-Q url through checking the file type in each table row
 url10Q = ''
@@ -33,27 +36,26 @@ for row in indexPage.findAll('tr'):
     if cell[4] == '10-Q':
         url10Q = 'https://www.sec.gov' + row.a.get('href')
         break
+logging.info('Find 10-Q url through checking the file type in each table row.')
 
 # extract all tables from 10-Q url and write the tables data in csv
 try:
     df = pd.read_html(url10Q, header = None) 
 except Exception:
-    print('Error, wrong 10-Q url, fail to get 10-Q file.')
-    
-# check cleaned directory    
-outdir = './cleaned_tables'
-if not os.path.exists(outdir):
-    os.mkdir(outdir)
+   logging.warning('Error, wrong 10-Q url, fail to get 10-Q file.')
 
 # extrac raw tables, clean up the data, and write cleaned tables to cleaned directory
 # since first four table has no useful data, start with table[4]
+logging.info('Extract raw tables, start to clean up data.')
 for i in range(4, len(df)):
     fullname = os.path.join(outdir, 'table' + str(i) + '.csv')    
     df1 = pd.DataFrame(df[i])
     
+    logging.info('Start to clean up Table' + str(i))
+
     # replace the $ sign in the table
     df1 = df1.replace('$',None)
-
+    
     # shift the row left whose firs column is empty
     s1 = df1[df1.columns[0]]
     for i,x in enumerate(s1):
@@ -68,4 +70,5 @@ for i in range(4, len(df)):
     df1.fillna('',inplace=True)
 
     # show the finished table and write to csv
-    df1.to_csv(fullname,index=False, header=None)  
+    df1.to_csv(fullname,index=False, header=None) 
+    logging.info('Write cleaned table' + str(i) + ' to csv.') 
